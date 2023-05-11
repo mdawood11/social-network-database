7cdb209a7c91f83216d2d6da2a24a8148fbacbb6
mohamad ghaznawy <mdawoodghaznawy@yahoo.com>
2023-05-10 17:02:57 -0700

    Modified data.ja in util folder

---
+++ util/data.js
@@ -0,0 +1,73 @@
+const getUserData = [
+  { username: "Alex", email: "alex@gmail.com" },
+  { username: "Mike", email: "mike@gmail.com" },
+  { username: "Farid", email: "farid@gmail.com" },
+  { username: "Bob", email: "bob@gmail.com" },
+  { username: "Mohamad", email: "mohamad@gmail.com" },
+  { username: "Masih", email: "masih@gmail.com" },
+  { username: "John", email: "john@gmail.com" },
+  { username: "James", email: "james@gmail.com" },
+  { username: "Missi", email: "missi@gmail.com" },
+];
+
+const getThoughtData = [
+  {
+    thoughtText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+    username: "Alex",
+  },
+  {
+    thoughtText:
+      "Aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "Mike",
+  },
+  {
+    thoughtText:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    username: "Farid",
+  },
+  {
+    thoughtText:
+      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "Bob",
+  },
+  {
+    thoughtText:
+      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    username: "Mohamad",
+  },
+  {
+    thoughtText:
+      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    username: "Masih",
+  },
+  {
+    thoughtText:
+      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.",
+    username: "John",
+  },
+  {
+    thoughtText:
+      "Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.",
+    username: "James",
+  },
+  {
+    thoughtText:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    username: "Missi",
+  },
+];
+
+const getReactionData = [
+  { username: "Alex", reactionBody: "Great job" },
+  { username: "Mike", reactionBody: "Nice" },
+  { username: "Farid", reactionBody: "Wow" },
+  { username: "Bob", reactionBody: "Awesome" },
+  { username: "Mohamad", reactionBody: "Super" },
+  { username: "Masih", reactionBody: "Good short" },
+  { username: "John", reactionBody: "Intrestin" },
+  { username: "James", reactionBody: "Cool" },
+  { username: "Missi", reactionBody: "Amazing" },
+];
+
+module.exports = { getUserData, getThoughtData, getReactionData };
